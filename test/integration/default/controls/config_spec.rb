81bb9db9146115fb879f1767e6109774d6ae8317
Nicolas Rodriguez <nicoladmin@free.fr>
2019-10-03 23:41:03 +0200

    test(inpsec): test apt-cacher client config

--- test/integration/default/controls/config_spec.rb
+++ test/integration/default/controls/config_spec.rb
@@ -8,4 +8,10 @@ control 'AptCacherNG configuration' do
     # Custom config
     its('content') { should include "Port: 9999" }
   end
+
+  describe file('/etc/apt/apt.conf.d/80proxy') do
+    # Default config
+    its('content') { should include 'Acquire::http::Proxy "http://localhost:9999";' }
+    its('content') { should include 'Acquire::https::Proxy "DIRECT";' }
+  end
 end
